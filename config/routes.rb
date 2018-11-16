74999b121d53d7c104b0440ca7ef5cbcd756aab4
Piera.celis <piera.celis@gmail.com>
2018-11-15 21:04:16 -0300

    Devise 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :installs
   get 'welcome/index'
   get 'contenido/entradas'
+  root to: "welcome#index"
  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
